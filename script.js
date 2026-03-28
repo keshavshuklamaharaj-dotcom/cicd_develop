3f9ea7a87229d328c47c15525d2fb34d4b9dcad6
Keshav Shukla <kshukla@invession.com>
2026-03-28 13:11:54 +0530

    first commit

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ const navLinks = document.querySelector('.nav-links');
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
-    
     hamburger.classList.toggle('active');
 });
 
